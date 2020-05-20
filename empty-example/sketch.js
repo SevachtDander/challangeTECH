eda3531fbd7ccc7518ff8bc0f5dd5d9b31d01cd1
Sander Devacht <sander.devacht@student.howest.be>
2020-05-20 15:18:31 +0200

    stuck bij de z index

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -1,5 +1,5 @@
 function setup() {
-  createCanvas(100, 900);
+  createCanvas(000, 000);
 
   // Create an Audio input
   mic = new p5.AudioIn();
@@ -15,17 +15,23 @@ function draw() {
 
   // Get the overall volume (between 0 and 1.0)
   let vol = mic.getLevel();
+  console.log(vol);
   
   if (vol>0.2)
   {
     playviddy();
     document.querySelector(".penis").innerHTML ="Almost the voice of a trash metal god";
+    document.querySelector(".flames").style.visibility="visible"
+    document.querySelector(".video_drummer").style.visibility="visible"
+    document.querySelector('.video_drummer').playbackRate = 5.0;
  
   }
   if (vol<0.01)
   {
     pauzeviddy();
     document.querySelector(".penis").innerHTML ="Train your metal voice";
+    document.querySelector(".flames").style.visibility="hidden"
+    document.querySelector(".video_drummer").style.visibility="hidden"
  
   }
   
@@ -34,16 +40,20 @@ function draw() {
 
   // Draw an ellipse with height based on volume
   let h = map(vol, 0, 1, height, 0);
-  ellipse(width / 2, h - 25, 50, 50);
+  ellipse(width / 2, h - 25, 00, 00);
 }
 
 function playviddy()
 {
   viddy = document.querySelector(".videoke");
   viddy.play();
+  drummerke=document.querySelector(".video_drummer");
+  drummerke.play();
 }
 function pauzeviddy()
 {
   viddy = document.querySelector(".videoke");
   viddy.pause();
+  drummerke=document.querySelector(".video_drummer");
+  drummerke.pause();
 }
